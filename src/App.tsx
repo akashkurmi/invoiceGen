088d82a3898a128c5092e84a86b0d4d0c8b430e5
Akash <kash@Akashs-MacBook-Pro.local>
2026-03-22 00:40:41 +0530

    pdf gen

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import "./App.css";
 import GayatriCollection from "./comp/Template/gayatriCollection.js";
 import AnjalMakeoverInvoice from "./comp/templateNewClaud.js";
